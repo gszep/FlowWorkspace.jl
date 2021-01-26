2f57f313b0378856914038a638217fc787e24068
gszep <gregory.szep@gmail.com>
2021-01-26 01:12:50 +0100

    fix file paths

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using FlowWorkspace
 using Test
 
 ######################################################### load single file
-workspace = "data/workspace.wsp"
-sample = "data/fcs/101_DEN084Y5_15_E01_008_clean.fcs"
+workspace = "./data/workspace.wsp"
+sample = "./data/fcs/101_DEN084Y5_15_E01_008_clean.fcs"
 
 data, = load(sample)
 data,labels,groups,gating = load(sample; workspace=workspace)
 
 ######################################################### multiple files
-pattern = glob"data/fcs/*.fcs"
+pattern = glob"./data/fcs/*.fcs"
 data,labels,groups,gating = load(pattern; workspace=workspace)
