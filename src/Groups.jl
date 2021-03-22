5aa3dd07f7d4f7d9582e82e552e86a046020b06e
gszep <gregory.szep@gmail.com>
2021-03-22 17:20:16 +0100

    closes #7

--- src/Groups.jl
+++ src/Groups.jl
@@ -5,7 +5,7 @@ function loadGroups(path::String, workspace::String, data::DataFrame)
     if sample !== nothing
 
         return DataFrame([ (group["name"]=>fill(true,size(data,1))) for group ∈ 
-            findall("""//SampleRefs/SampleRef[contains(@sampleID,'$(sample["sampleID"])')]/../..""",workspace)
+            findall("""//SampleRefs/SampleRef[@sampleID='$(sample["sampleID"])']/../..""",workspace)
                 if  group["name"] ≠ "All Samples" ])
     else 
         return nothing
